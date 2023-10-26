21b44f56fa42cee3e11586a5a8a9852fc81f4963
Yammir Tamayo <149030231+9figuretraderfx@users.noreply.github.com>
2023-10-25 20:42:25 -0700

    Add files via upload
    suma de numeros

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int num, suma = 0;
+
+        
+          do  {
+            Console.WriteLine("Ingresa un numero");
+
+            num = int.Parse(Console.ReadLine());
+                suma = suma + num;
+        } while (num != 0) ;
+
+        {
+            Console.WriteLine("Tu suma es: " + suma);
+            
+        }
+    }
+}
+        
