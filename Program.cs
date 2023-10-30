830a15b9693087415cf06948ff497b49a0023f4e
Yammir Tamayo <149030231+9figuretraderfx@users.noreply.github.com>
2023-10-29 21:33:37 -0700

    Add files via upload
    Estrellas descendentes

--- Program.cs
+++ Program.cs
@@ -4,20 +4,24 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        int num, suma = 0;
-
-        
-          do  {
-            Console.WriteLine("Ingresa un numero");
-
-            num = int.Parse(Console.ReadLine());
-                suma = suma + num;
-        } while (num != 0) ;
-
+        int i, j;
+        for (i = 1; i <= 10; i++)
         {
-            Console.WriteLine("Tu suma es: " + suma);
-            
+            {
+                for (j = 1; j <=i; j++)
+                {
+                    Console.Write("*");
+                } Console.WriteLine();
+            }
         }
     }
 }
-        
+
+                
+       
+
+
+
+
+
+
