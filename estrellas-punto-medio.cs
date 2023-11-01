4bf9bb7337630cfa73f8121325424d7b077d9a1a
Yammir Tamayo <149030231+9figuretraderfx@users.noreply.github.com>
2023-11-01 13:35:40 -0700

    Add files via upload

---
+++ estrellas-punto-medio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int i, j;
+        for (i = 1; i <= 5; i++)
+        {
+            for (j = 1; j <= i; j++)
+            {
+                for (i = 4; i <= 1; i--)
+                {
+                    for (j = 4; j <= i; j++)
+                    {
+                        Console.Write("*");
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
