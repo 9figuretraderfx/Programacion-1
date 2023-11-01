5af39c92e9e70a1b6e9e4a382c632288075405ba
Yammir Tamayo <149030231+9figuretraderfx@users.noreply.github.com>
2023-11-01 13:30:36 -0700

    Add files via upload

---
+++ programacion-6-veces.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int i = 1;
+
+        while (i <=6)
+        {
+
+            Console.WriteLine("Programacion"); 
+            i++;
+
+        }
+    }
+}
+
+
