482ce52bddf4370d51a47eb932c50c320f23f7f4
Yammir Tamayo <149030231+9figuretraderfx@users.noreply.github.com>
2023-11-01 13:43:16 -0700

    Add files via upload

---
+++ estrellas-ascendentes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int i, j;
+        for (i = 1; i <= 10; i++)
+        {
+            {
+                for (j = 1; j <=i; j++)
+                {
+                    Console.Write("*");
+                } Console.WriteLine();
+            }
+        }
+    }
+}
+
+                
+       
+
+
+
+
+
+
