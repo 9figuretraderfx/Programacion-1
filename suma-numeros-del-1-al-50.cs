f0d907512e0aad314c2f0e942bee834e54c043cb
Yammir Tamayo <149030231+9figuretraderfx@users.noreply.github.com>
2023-11-01 13:19:22 -0700

    Add files via upload

---
+++ suma-numeros-del-1-al-50.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int num = 0;
+        for (int i = 1; i <=50 ; i++)
+        {
+            num = i * (i + 1) / 2;
+        }
+
+        Console.WriteLine("La suma de los numeros del 1 al 50 es: " + num);
+        Console.Read();
+    }
+}
+
+        
