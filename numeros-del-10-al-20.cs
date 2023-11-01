fdd4db2afed368bae0762dde5daf9358084abf60
Yammir Tamayo <149030231+9figuretraderfx@users.noreply.github.com>
2023-11-01 13:17:05 -0700

    Add files via upload

---
+++ numeros-del-10-al-20.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int num;
+        int i = 10;
+
+        while (i<= 20) 
+            {
+
+            Console.WriteLine(" " + i);
+            i++;
+            
+        }
+    }
+}
