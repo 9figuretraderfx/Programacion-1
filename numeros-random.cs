c20cb156c72481c7da6f8d0f2c217641989acf4a
Yammir Tamayo <149030231+9figuretraderfx@users.noreply.github.com>
2023-11-01 20:45:08 -0700

    Add files via upload

---
+++ numeros-random.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = new int[10];
+        Random rand = new Random();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i]= rand.Next(1, 100);
+
+            Console.WriteLine(nums[i]);
+        }
+    }
+}
