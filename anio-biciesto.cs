10ecea6bc6db9f3123137e530dd311addc21c912
Yammir Tamayo <149030231+9figuretraderfx@users.noreply.github.com>
2023-11-01 20:26:17 -0700

    Add files via upload

---
+++ anio-biciesto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int anio = 0;
+
+        int[] aniobiciesto = new int[1];
+
+        Console.WriteLine("Ingrese el anio a evaluar");
+        anio = int.Parse(Console.ReadLine());
+
+        if (anio % 4 != 0)
+            Console.WriteLine("No es biciesto");
+
+        if (anio % 4 == 0 && anio % 100 != 0)
+            Console.WriteLine("Es biciesto");
+
+        if (anio % 4 == 0 && anio % 100 == 0 && anio % 400 == 0)
+            Console.WriteLine("Es biciesto");
+
+        else if (anio % 4 == 0 && anio % 100 == 0 && anio % 400 != 0)
+            Console.WriteLine("No es biciesto");
+    }
+}
+
+
+
+
+
+
+
+
