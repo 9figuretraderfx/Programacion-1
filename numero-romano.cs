6ae08bed5e9d9d9491d072423cfac98fe29eb6c9
Yammir Tamayo <149030231+9figuretraderfx@users.noreply.github.com>
2023-11-08 12:13:02 -0800

    Add files via upload

---
+++ numero-romano.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un numero entre el 1 y el 5");
+
+        int numero;
+        numero = Convert.ToInt16(Console.ReadLine());
+
+        switch (numero)
+        {
+            case 1:
+                Console.WriteLine("Numero romano: I ");
+                break;
+            case 2:
+                Console.WriteLine("Numero romano: II ");
+                break;
+            case 3:
+                Console.WriteLine("Numero romano: III: ");
+                break;
+            case 4:
+                Console.WriteLine("Numero romano: IV ");
+                break;
+            case 5:
+                Console.WriteLine("Numero romano: V ");
+                break;
+            default:
+                Console.WriteLine("Ingrese un numero entre el 1 y el 5");
+                break;
+        }
+    }
+}
