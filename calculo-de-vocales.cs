7007081fca5c20710151bb918b9520b5a97c18e3
Yammir Tamayo <149030231+9figuretraderfx@users.noreply.github.com>
2023-11-29 11:42:37 -0800

    Add files via upload

---
+++ calculo-de-vocales.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+public class Program
+{
+    static void A(int vocales)
+    {
+        int numVocales = 0;
+        Console.WriteLine($"La palabra tiene {numVocales} vocales A");
+    }
+
+    static void E(int vocales)
+    {
+        int numVocales = 0;
+        Console.WriteLine($"La palabra tiene {numVocales} vocales E");
+    }
+
+    static void I(int vocales)
+    {
+        int numVocales = 0;
+        Console.WriteLine($"La palabra tiene {numVocales} vocales I");
+    }
+
+    static void O(int vocales)
+    {
+        int numVocales = 0;
+        Console.WriteLine($"La palabra tiene {numVocales} vocales O");
+    }
+
+    static void U(int vocales)
+    {
+        int numVocales = 0;
+        Console.WriteLine($"La palabra tiene {numVocales} vocales U");
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Escribe una palabra: ");
+        string palabra = Console.ReadLine().ToLower();
+        int numVocales = 0;
+
+        for (int i = 0; i < palabra.Length; i++)
+        {
+            if (palabra[i] == 'a' || palabra[i] == 'e' || palabra[i] == 'i' || palabra[i] == 'o' || palabra[i] == 'u')
+            {
+                numVocales++;
+            }
+        }
+        Console.WriteLine($"La cantidad de vocales en la palabra es de: " + numVocales);
+        Console.ReadLine();
+    }
+}
+
+        
+
+
+
+
+
