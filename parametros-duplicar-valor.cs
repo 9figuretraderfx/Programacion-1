453787b09200cc4240a4e19563040dcfb95ea744
Yammir Tamayo <149030231+9figuretraderfx@users.noreply.github.com>
2023-12-03 17:26:44 -0800

    Add files via upload

---
+++ parametros-duplicar-valor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    static int incrementarReferencia(int x)
+    {
+        int numIncrementado = (x + 1);
+        return numIncrementado;
+
+    }
+
+    public static void Main(string[] args)
+    {
+
+        string[] numeros = new string[5];
+        int x = int.Parse(Console.ReadLine()); 
+        x = incrementarReferencia(x);
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Ingresa un numero");
+            numeros[i] = Console.ReadLine();
+        }
+        
+        Console.WriteLine($"Los numeros incrementados son {x}");
+        
+    }
+}
+
