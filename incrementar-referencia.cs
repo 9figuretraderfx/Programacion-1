de24a4a04743518f4766e84bab7ff5ed3a159a47
Yammir Tamayo <149030231+9figuretraderfx@users.noreply.github.com>
2023-12-05 23:31:57 -0800

    Add files via upload

---
+++ incrementar-referencia.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int numero = 0;
+        Console.WriteLine("Ingrese un numero");
+        numero = int.Parse(Console.ReadLine());
+        incrementarReferencia(ref numero);
+        Console.WriteLine("Metodo main: " + numero);
+    }
+
+    static void incrementarReferencia(ref int numero)
+    {
+        numero = numero + 1;
+        Console.WriteLine("Metodo: " + numero);
+    }
+}
+
+
+
