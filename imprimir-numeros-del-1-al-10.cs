bc364d1ab49d8e8c0b2c58ceacc39f7781be8a46
Yammir Tamayo <149030231+9figuretraderfx@users.noreply.github.com>
2023-12-12 23:55:45 -0800

    Add files via upload

---
+++ imprimir-numeros-del-1-al-10.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+
+            Console.WriteLine(i + "\t");
+            Console.ReadKey();
+        }
+    }
+}
+
+
